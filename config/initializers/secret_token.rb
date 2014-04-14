184a6977d68aacfc2408a5843573749fe4eb226c
Mikolaj Kaminski <ferozstein@gmail.com>
2014-04-14 22:35:21 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gw2gourmet::Application.config.secret_key_base = 'abc17b979041ba30997565caa19536d5673eafddee35a86b219343e7eac351d0f53483c4a8931af63ba17587809a98aa42bd5a5157d33ecfea0e53a4a28b4789'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gw2gourmet::Application.config.session_store :cookie_store, key: '_gw2gourmet_session'
