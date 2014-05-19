f1adaab817b6877eae2aea86562d9a097b982686
Mikolaj Kaminski <ferozstein@gmail.com>
2014-05-19 13:43:54 +0200

    poprawka dodawania komentarzy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,8 +2,16 @@ class CommentsController < ApplicationController
   
   def add
 
-  	@comment = 
+  	@comment = Comment.new(comment_params)
+  	# Comment.create(body: params[:body], user_id: params[:user_id], item_id: params[:item_id])
+  	
 
+  end
+
+  private
+
+  def comment_params
+  	params.require(:comment).permit(:body, :user_id, :item_id)
   end
 
 
